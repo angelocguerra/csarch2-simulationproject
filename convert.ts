167b5b306863281e45774c09a4d8c2b948f753fa
ArigatYungch <aids_yungcheng@dlsu.edu.ph>
2024-03-16 20:34:30 +0800

    Created rough versions of typescript functions for decimal to binary conversions and binary normalizations

---
+++ convert.ts
@@ -0,0 +1,65 @@
+function decToBin(decimal: number): number {
+    let integer = Math.floor(decimal);
+    let fractional = decimal - integer;
+    let binary = "";
+
+    while (integer > 0) {
+        binary = (integer % 2) + binary;
+        integer = Math.floor(integer / 2);
+    }
+
+    if (fractional > 0) {
+        binary += ".";
+        while (fractional > 0) {
+            fractional *= 2;
+            let bit = Math.floor(fractional);
+            binary += bit.toString();
+            fractional -= bit;
+        }
+    }
+
+    return parseFloat(binary);
+}
+
+function checkBinary(input: string): boolean {
+    for (let digit of input) {
+        if (digit !== "0" && digit !== "1") {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function normalize(input: number, exp: number): [number, number] {
+    console.log(input, exp);
+    while (input > 2) {
+        input /= 10;
+        exp += 1;
+        console.log(input, exp);
+    }
+
+    return [parseFloat(input.toFixed(2)), exp];
+}
+
+function converter(entry: number, base: number, exp: number): [number, number, number] {
+    if (base === 10) {
+        if (exp > 0) {
+            while(exp > 0) {
+                entry *= 10;
+                exp -= 1;
+            }
+        } else {
+            while(exp < 0) {
+                entry /= 10;
+                exp += 1;
+            }
+        }
+        entry = decToBin(entry);
+        base = 2;
+    }
+
+    [entry, exp] = normalize(entry, exp);
+
+    return [entry, base, exp];
+}
